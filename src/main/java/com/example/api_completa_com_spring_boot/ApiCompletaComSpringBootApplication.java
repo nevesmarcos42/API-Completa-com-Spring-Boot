c4c9e787de8e2d1df893be5d855ab490999e7e33
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-07-04 12:56:52 -0300

    init: projeto gerado com Spring Initializr

---
+++ src/main/java/com/example/api_completa_com_spring_boot/ApiCompletaComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_completa_com_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompletaComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCompletaComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_completa_com_spring_boot/ApiCompletaComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_completa_com_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCompletaComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
